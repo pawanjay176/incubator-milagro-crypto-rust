d75bd4a1b39cee30f6f35e82aa833036fabc8dc3
Kirk Baird <baird.k@outlook.com>
2019-08-15 09:31:41 +1000

    SHA256 HKDF
    Signed-off-by: Kirk Baird <baird.k@outlook.com>

--- src/hash256.rs
+++ src/hash256.rs
@@ -260,33 +260,29 @@ impl HASH256 {
     /// HKDF-Extend
     ///
     /// https://tools.ietf.org/html/rfc5869
-    pub fn hkdf_extend(prk: &[u8], info: &[u8], l: usize) -> Vec<u8> {
-        assert!(l <= 255 * HASH_BYTES, "Length can be at most 255 bytes");
-        // n the number of concatenated hashes
+    pub fn hkdf_extend(prk: &[u8], info: &[u8], l: u8) -> Vec<u8> {
         // n = cieling(l / 32)
-        let mut n = l / HASH_BYTES;
-        println!("n: {}", n);
-        if n * HASH_BYTES < l {
+        let mut n = l / (HASH_BYTES as u8);
+        if n * (HASH_BYTES as u8) < l {
             n += 1;
-            println!("n += 1: {}", n);
         }
 
         let mut okm: Vec<u8> = vec![];
-        let mut previous = [0u8; HASH_BYTES];
+        let mut previous = vec![]; // T(0) = []
 
-        for i in 0..n {
-            // Concatenate (previous || info || i)
-            let mut text = previous.to_vec();
+        for i in 0..n as usize {
+            // Concatenate (T(i) || info || i)
+            let mut text: Vec<u8> = previous;
             text.extend_from_slice(info);
-            text.push(i as u8); // Note: n <= 255
+            text.push((i + 1) as u8); // Note: i <= 254
 
-            previous = HASH256::hmac(prk, &text);
+            // T(i+1) = HMAC(PRK, T(i) || info || i)
+            previous = HASH256::hmac(prk, &text).to_vec();
             okm.extend_from_slice(&previous);
         }
 
-        // Reduce length to size l
-        okm.resize(l, 0);
-
+        // Reduce length to size L
+        okm.resize(l as usize, 0);
         okm
     }
 }
@@ -338,8 +334,8 @@ mod tests {
     #[test]
     fn test_hmac_32_byte_key() {
         let text = [0x0a];
-        let key = hex::decode("abababababababababababababababababababababababababababababababab").unwrap();
-        println!("Key {}", key.len());
+        let key = hex::decode("abababababababababababababababababababababababababababababababab")
+            .unwrap();
         let expected =
             hex::decode("43997a72e7b3b1c19e5566c940d5f2961c96802b58a3da2acd19dcc1a90a8d05")
                 .unwrap();
@@ -389,17 +385,68 @@ mod tests {
         // From https://tools.ietf.org/html/rfc5869
         let ikm = hex::decode("0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b").unwrap();
         let salt = hex::decode("000102030405060708090a0b0c").unwrap();
-        let expected_prk = hex::decode("077709362c2e32df0ddc3f0dc47bba6390b6c73bb50f9c3122ec844ad7c2b3e5").unwrap();
+        let expected_prk =
+            hex::decode("077709362c2e32df0ddc3f0dc47bba6390b6c73bb50f9c3122ec844ad7c2b3e5")
+                .unwrap();
 
         let output_prk = HASH256::hkdf_extract(&salt, &ikm).to_vec();
         assert_eq!(expected_prk, output_prk);
 
         let info = hex::decode("f0f1f2f3f4f5f6f7f8f9").unwrap();
         let l = 42;
-        let expected_okm = hex::decode("3cb25f25faacd57a90434f64d0362f2a2d2d0a90cf1a5a4c5db02d56ecc4c5bf34007208d5b887185865").unwrap();
+        let expected_okm = hex::decode(
+            "3cb25f25faacd57a90434f64d0362f2a2d2d0a90cf1a5a4c5db02d56ecc4c5bf34007208d5b887185865",
+        )
+        .unwrap();
 
         let output_okm = HASH256::hkdf_extend(&expected_prk, &info, l);
-        println!("OKM Len: {}", output_okm.len());
+        assert_eq!(expected_okm, output_okm);
+    }
+
+    #[test]
+    fn test_hkdf_case_2() {
+        // From https://tools.ietf.org/html/rfc5869
+        let ikm = hex::decode("000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f202122232425262728292a2b2c2d2e2f303132333435363738393a3b3c3d3e3f404142434445464748494a4b4c4d4e4f")
+            .unwrap();
+        let salt = hex::decode("606162636465666768696a6b6c6d6e6f707172737475767778797a7b7c7d7e7f808182838485868788898a8b8c8d8e8f909192939495969798999a9b9c9d9e9fa0a1a2a3a4a5a6a7a8a9aaabacadaeaf")
+            .unwrap();
+        let expected_prk =
+            hex::decode("06a6b88c5853361a06104c9ceb35b45cef760014904671014a193f40c15fc244")
+                .unwrap();
+
+        let output_prk = HASH256::hkdf_extract(&salt, &ikm).to_vec();
+        assert_eq!(expected_prk, output_prk);
+
+        let info = hex::decode("b0b1b2b3b4b5b6b7b8b9babbbcbdbebfc0c1c2c3c4c5c6c7c8c9cacbcccdcecfd0d1d2d3d4d5d6d7d8d9dadbdcdddedfe0e1e2e3e4e5e6e7e8e9eaebecedeeeff0f1f2f3f4f5f6f7f8f9fafbfcfdfeff")
+            .unwrap();
+        let l = 82;
+        let expected_okm = hex::decode("b11e398dc80327a1c8e7f78c596a49344f012eda2d4efad8a050cc4c19afa97c59045a99cac7827271cb41c65e590e09da3275600c2f09b8367793a9aca3db71cc30c58179ec3e87c14c01d5c1f3434f1d87")
+            .unwrap();
+
+        let output_okm = HASH256::hkdf_extend(&expected_prk, &info, l);
+        assert_eq!(expected_okm, output_okm);
+    }
+
+    #[test]
+    fn test_hkdf_case_3() {
+        // From https://tools.ietf.org/html/rfc5869
+        let ikm = hex::decode("0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b").unwrap();
+        let salt = vec![];
+        let expected_prk =
+            hex::decode("19ef24a32c717b167f33a91d6f648bdf96596776afdb6377ac434c1c293ccb04")
+                .unwrap();
+
+        let output_prk = HASH256::hkdf_extract(&salt, &ikm).to_vec();
+        assert_eq!(expected_prk, output_prk);
+
+        let info = vec![];
+        let l = 42;
+        let expected_okm = hex::decode(
+            "8da4e775a563c18f715f802a063c5a31b8a11f5c5ee1879ec3454e5f3c738d2d9d201395faa4b61a96c8",
+        )
+        .unwrap();
+
+        let output_okm = HASH256::hkdf_extend(&expected_prk, &info, l);
         assert_eq!(expected_okm, output_okm);
     }
 }
